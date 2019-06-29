be810a888865289fc43989b6f383f4a7c4d5c120
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-29 21:12:24 +0300

    Creating an Entity Framework Model

---
+++ NijaDomain.DataModel/NinjaContext.cs
@@ -0,0 +1,12 @@
+﻿using NijaDomain.Classes;
+using System.Data.Entity;
+
+namespace NijaDomain.DataModel
+{
+    public class NinjaContext : DbContext
+    {
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+    }
+}
