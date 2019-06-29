df1b4ff222f81f339bbe0edf2a7c364d3a5e107b
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-29 23:37:12 +0300

    Retrieving Data with the Find and SqlQuery Methods

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -122,5 +122,34 @@ namespace ConsoleApp2
                 context.SaveChanges();
             }
         }
+
+        private static void RetrieveDataWithFind()
+        {
+            var keyval = 4;
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                var ninja = context.Ninjas.Find(keyval);
+                Console.WriteLine("After Find#1:" + ninja.Name);
+
+                var someNinja = context.Ninjas.Find(keyval);
+                Console.WriteLine("After Find#2:" + someNinja.Name);
+                ninja = null;
+            }
+        }
+
+        private static void RetrieveDataWithStoredProc()
+        {
+
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                var ninjas = context.Ninjas.SqlQuery("exec GetOldNinjas").ToList();
+                //foreach (var ninja in ninjas)
+                //{
+                //    Console.WriteLine(ninja.Name);
+                //}
+            }
+        }
     }
 }
---
+++ dbo.Procedure.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE GetOldNinjas
+	
+AS
+	SELECT * From Ninjas where DateOfBirth<='03/12/1994'
+RETURN 0
