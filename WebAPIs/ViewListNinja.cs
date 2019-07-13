1ff95908f633e4d0381167c2f3d04d852fa47f3a
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-07-13 19:04:53 +0300

    Entity Framework and ASP.NET Web API

---
+++ WebAPIs/ViewListNinja.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebAPIs
+{
+    public class ViewListNinja
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool ServedInOniwaban { get; set; }
+        public string ClanName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
